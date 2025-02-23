3c888c4581a6319af75fc489fdb3d6c1803957f6
Etienne Grosdidier <141655132+egrosdidier@users.noreply.github.com>
2025-02-23 20:24:12 +0100

    Update methadone_monitoring.py

--- methadone_monitoring.py
+++ methadone_monitoring.py
@@ -38,8 +38,8 @@ def plot_methadone_curves(dose, weight, half_life, time_since_last_dose, methado
     patient_concentrations = [calculate_methadone_concentration(dose, weight, half_life, t, steady_state=False) for t in time]
     
     fig, ax = plt.subplots()
-    ax.plot(time, expected_concentrations, label="Courbe attendue (modèle)", color='blue')
-    ax.plot(time, patient_concentrations, label="Courbe du patient (observée)", linestyle='dashed', color='gray')
+    ax.plot(time, expected_concentrations, label="Courbe du patient (observée)", linestyle='dashed', color='gray')
+    ax.plot(time, patient_concentrations, label="Courbe attendue (modèle)", color='blue')
     ax.axhline(100, color='green', linestyle='--', label='Seuil bas (100 ng/mL)')
     ax.axhline(400, color='blue', linestyle='--', label='Zone thérapeutique (400 ng/mL)')
     ax.axhline(600, color='red', linestyle='--', label='Risque de toxicité (600 ng/mL)')
@@ -58,7 +58,7 @@ st.title("Évaluation de la Méthadonémie")
 
 dose = st.number_input("Dose quotidienne de méthadone (mg)", min_value=1, max_value=300, value=60)
 weight = st.number_input("Poids du patient (kg)", min_value=30, max_value=150, value=70)
-half_life = st.slider("Demi-vie de la méthadone (h)", min_value=10, max_value=60, value=24)
+half_life = st.slider("Demi-vie de la méthadone (h)", min_value=10, max_value=60, value=36)
 time_since_last_dose = st.number_input("Temps depuis la dernière prise (h)", min_value=1, max_value=48, value=12)
 methadone_measured = st.number_input("Méthadonémie mesurée (ng/mL)", min_value=0, max_value=2000, value=350)
 eddp_measured = st.number_input("EDDP mesuré (ng/mL)", min_value=0, max_value=2000, value=120)
